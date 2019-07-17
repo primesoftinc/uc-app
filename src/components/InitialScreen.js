98105555a4f48b029271402dbed01c262851a3b5
mounika <mounika.p@alphaclinicalsystems.com>
2019-07-17 02:45:52 -0400

    changed background to red.

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -12,7 +12,7 @@ export default function InitialScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
