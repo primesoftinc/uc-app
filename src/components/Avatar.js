7ac9f8e01cbf0e5a409e86b562b89852eaf75bb3
jayalakshmi <you@example.com>
2019-07-27 10:40:06 +0530

    Updated Avatar Component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,49 +1,53 @@
 import React ,{Component}from 'react';
-import {Text,View,StyleSheet} from 'react-native';
-import {Avatar,Badge} from 'react-native-elements';
-
-const avat ={
+import {Image,View,StyleSheet,Alert} from 'react-native';
+const avat={
+        small:{
+            height:50,
+            width:50
+        },
+        medium:{
+            height:75,
+            width:75
+        },
+        large:{
+            height:100,
+            width:100
+        },
+        Xlarge:{
+            height:125,
+            width:125
+        }
+}
+const avatStatus={
     online:{
-       
-        borderColor:'#0fda1e'
+        borderColor:'#42f563'
     },
     offline:{
-       
-        borderColor:'red'
+        borderColor:'#f54842'
     },
     busy:{
-        borderColor:'#f5b041'
+        borderColor:'#fcc705'
     }
 }
+export default class Avatar extends Component{
+   
+    render(){
+        const {status,size} = this.props
+        return(<View>
+            <Image
+                style={[styles.imageStyle,avat[size],avatStatus[status]]}
+                source={{uri:  'https://images.pexels.com/photos/42273/doctor-medical-medicine-health-42273.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+                }}/>
+        </View>
 
-
-export default class AvatarComponent extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){ 
-        const {status,size,styl} = this.props;
-        console.log(status);
-        return(
-            <View><Text>Avatar!!!</Text>
-            <View>
-            </View>
-                <Avatar 
-                rounded
-                source={{
-                    uri: 'https://randomuser.me/api/portraits/men/41.jpg',
-                    
-                }}
-                size={size}
-                avatarStyle={[styles.default,avat[status]]}
-                />
-            </View>
         );
     }
 }
-const styles=StyleSheet.create({
-    default:{
-        borderWidth:1.5,
-        borderRadius:100,
-    }
-})
+const styles = StyleSheet.create({
+    imageStyle: {
+        borderRadius:50,
+        height:75,
+        width:75,
+        borderWidth:2
+}
+});
