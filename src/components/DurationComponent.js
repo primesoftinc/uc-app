5558c935a454382f08b9f7a5497b3f5010d4ac81
jayalakshmi <you@example.com>
2019-08-02 18:57:06 +0530

    Modified the component to accept props

--- src/components/DurationComponent.js
+++ src/components/DurationComponent.js
@@ -1,20 +1,36 @@
-
 import React,{Component} from 'react';
-import {View} from 'react-native';
+import {View,Dimensions,Text} from 'react-native';
 import InputSpinner from 'react-native-input-spinner';
+var { width} = Dimensions.get('window');
+
 export default class TextInputExample extends Component{
+    
     render(){
-        return(
-            <View>
-            <InputSpinner
-    max={100}
-    min={1}
-    step={1}
-    color={'#590DF0'}
-    value={1}
-    Not rounded={false}
-    showBorder={true}
-    onChange={(num)=>{}} />
-        </View>);
+        const {Symptoms='fever'} =this.props
+        return(<View style={{flexDirection:'row',width:width,height:100}}>
+            <View style={{flex:2,justifyContent:'center'}}>
+            <Text style={{fontSize:15,paddingLeft:10,fontWeight:'100'}}>{Symptoms}</Text>
+            </View>
+            <View style={{flex:4,justifyContent:'center',alignItems:'center'}}>
+                 
+                <InputSpinner
+                style={{borderColor:'#4e38fe',borderWidth:1}}
+                buttonTextColor={'#4e38fe'}
+                buttonFontSize={16}
+                activeOpacity={0.5}
+                // buttonStyle={{borderColor:'#4e38fe',borderWidth:1}}
+                // textColor={'#bfbfbf'}
+                    max={100}
+                    min={0}
+                    step={1}
+                    color={'#ffffff'}
+                    value={1}
+                    Not rounded={false}
+                    showBorder={true}
+                    onChange={(num)=>{}} 
+                />
+    </View>
+    </View>
+        );
     }
 }
