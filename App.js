57cf8ea377b7b316bea0e80bea621ea21d986f34
mounika <mounika.p@alphaclinicalsystems.com>
2019-08-22 15:38:50 +0530

    added ApolloProvider

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
-import AppNavigator from './src/containers/AppNavigator';
-
+import React, { Component } from "react";
+import AppNavigator from "./src/containers/AppNavigator";
+import client from "./src/util/apollo";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 export default function App() {
   return (
+    <ApolloProvider client={client}>
       <AppNavigator />
+    </ApolloProvider>
   );
 }
-
